c65feb41a54bba754bc270301905883025fcd2b5
Billy <vanhove.billy@gmail.com>
2021-05-12 16:58:40 +0200

    cin cout

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char antwoord[512];
+
+    cout << "Geef je antwoord op: ";
+    cin.getline(antwoord,512);
+
+    cout << "je gegeven antwoord was: " << antwoord;
 
     return 0;
 }
